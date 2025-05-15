ebf25948bf704cffee9cca2c45bc4196bb4804b0
gszep <grisha.szep@gmail.com>
2025-05-16 01:13:16 +0900

    change point model finally works!

--- tests/test_changepoint_model.py
+++ tests/test_changepoint_model.py
@@ -4,15 +4,17 @@ GenJAX changepoint model example from https://www.gen.dev/tutorials/intro-to-mod
 
 from abc import ABC
 from dataclasses import dataclass, field
+from functools import partial
 
 import jax
+from jax import jit, vmap
 import jax.numpy as jnp
 from genjax import beta, flip, gen, scan, normal, Trace  # type: ignore
-from jaxtyping import Array, Float, Bool, Integer
+from jaxtyping import Array, Float, Bool, Integer, PRNGKeyArray
 
 from matplotlib import pyplot as plt
 
-MAX_DEPTH = 5
+MAX_DEPTH = 2
 MAX_NODES = 2 ** (MAX_DEPTH + 1) - 1
 
 
@@ -26,7 +28,7 @@ class Interval:
 @jax.tree_util.register_dataclass
 @dataclass
 class Node(ABC):
-    pass
+    idx: int
 
 
 @jax.tree_util.register_dataclass
@@ -47,6 +49,7 @@ class LeafNode(Node):
 @jax.tree_util.register_dataclass
 @dataclass
 class TreeBuffer:
+
     lower: Float[Array, "MAX_NODES"]
     upper: Float[Array, "MAX_NODES"]
     values: Float[Array, "MAX_NODES"]
@@ -55,20 +58,25 @@ class TreeBuffer:
         default_factory=lambda: jnp.arange(MAX_NODES, dtype=jnp.int32)
     )
 
-    children: Integer[Array, "MAX_NODES 2"] = field(
-        default_factory=lambda: jnp.full((MAX_NODES, 2), -1, dtype=jnp.int32)
+    left_idx: Integer[Array, "MAX_NODES"] = field(
+        default_factory=lambda: jnp.where(
+            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 1 < MAX_NODES,
+            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 1,
+            -1,
+        )
     )
 
-    def __post_init__(self):
-        idx = jnp.arange(MAX_NODES, dtype=jnp.int32)
-
-        # begin with a full binary tree
-        self.children = jnp.stack([2 * idx + 1, 2 * idx + 2], axis=-1)
-        self.children = jnp.where(self.children < MAX_NODES, self.children, -1)
+    right_idx: Integer[Array, "MAX_NODES"] = field(
+        default_factory=lambda: jnp.where(
+            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 2 < MAX_NODES,
+            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 2,
+            -1,
+        )
+    )
 
     @property
     def is_leaf(self) -> Bool[Array, "MAX_NODES"]:
-        return (self.children == -1).all(axis=-1)
+        return (self.left_idx == -1) & (self.right_idx == -1)
 
 
 @gen
@@ -77,7 +85,16 @@ def leaf(buffer: TreeBuffer, idx: int) -> tuple[TreeBuffer, int]:
     value = normal(0.0, 1.0) @ f"value_{idx}"
     buffer.values = buffer.values.at[idx].set(value)
 
-    buffer.children = buffer.children.at[idx].set(-1)
+    buffer.left_idx = buffer.left_idx.at[idx].set(-1)
+    buffer.right_idx = buffer.right_idx.at[idx].set(-1)
+    return buffer, idx
+
+
+def _leaf(buffer: TreeBuffer, idx: int, value: float) -> tuple[TreeBuffer, int]:
+    buffer.values = buffer.values.at[idx].set(value)
+
+    buffer.left_idx = buffer.left_idx.at[idx].set(-1)
+    buffer.right_idx = buffer.right_idx.at[idx].set(-1)
     return buffer, idx
 
 
@@ -87,8 +104,22 @@ def branch(buffer: TreeBuffer, idx: int) -> tuple[TreeBuffer, int]:
 
     frac = beta(2.0, 2.0) @ f"beta_{idx}"
     midpoint = lower + frac * (upper - lower)
+    left, right = buffer.left_idx[idx], buffer.right_idx[idx]
 
-    left, right = buffer.children[idx]
+    buffer.lower = buffer.lower.at[left].set(lower)
+    buffer.upper = buffer.upper.at[left].set(midpoint)
+
+    buffer.lower = buffer.lower.at[right].set(midpoint)
+    buffer.upper = buffer.upper.at[right].set(upper)
+
+    return buffer, idx
+
+
+def _branch(buffer: TreeBuffer, idx: int, frac: float) -> tuple[TreeBuffer, int]:
+    lower, upper = buffer.lower[idx], buffer.upper[idx]
+
+    midpoint = lower + frac * (upper - lower)
+    left, right = buffer.left_idx[idx], buffer.right_idx[idx]
 
     buffer.lower = buffer.lower.at[left].set(lower)
     buffer.upper = buffer.upper.at[left].set(midpoint)
@@ -102,35 +133,48 @@ def branch(buffer: TreeBuffer, idx: int) -> tuple[TreeBuffer, int]:
 @scan(n=MAX_NODES)  # in breadth-first search order
 @gen
 def binary_tree(buffer: TreeBuffer, idx: int) -> tuple[TreeBuffer, int]:
+    args = (buffer, idx)
 
     frac = beta(2.0, 2.0) @ f"beta_{idx}"
     value = normal(0.0, 1.0) @ f"value_{idx}"
+    is_leaf = flip(0.5) @ f"is_leaf_{idx}"
 
-    buffer.values = buffer.values.at[idx].set(value)
+    buffer, idx = jax.lax.cond(
+        buffer.is_leaf[idx] | is_leaf,
+        lambda buffer, idx: _leaf(buffer, idx, value),
+        lambda buffer, idx: _branch(buffer, idx, frac),
+        *args,
+    )
 
-    lower, upper = buffer.lower[idx], buffer.upper[idx]
-    midpoint = lower + frac * (upper - lower)
-
-    left, right = buffer.children[idx]
-
-    buffer.lower = buffer.lower.at[left].set(lower)
-    buffer.upper = buffer.upper.at[left].set(midpoint)
-
-    buffer.lower = buffer.lower.at[right].set(midpoint)
-    buffer.upper = buffer.upper.at[right].set(upper)
-
-    # args = (buffer, idx)
-    # is_leaf = flip(0.7) @ f"is_leaf_{idx}"
     # buffer, idx = (
     #     leaf.or_else(branch)(
-    #         is_leaf | (jnp.floor(jnp.log2(idx + 1)) > MAX_DEPTH), args, args
+    #         is_leaf, args, args
     #     )
     #     @ f"leaf_or_else_branch_{idx}"
     # )
     return buffer, idx
 
 
+@jit
+@partial(vmap, in_axes=(0, None))
+def sample_binary_tree(key: PRNGKeyArray, buffer: TreeBuffer) -> Trace:
+    return binary_tree.simulate(key, args=(buffer, buffer.idx))
+
+
 def test_changepoint_model():
+    colors = ["red", "blue", "green", "orange"]
+    N = len(colors)
+
+    trace: Trace = sample_binary_tree(
+        jax.random.split(jax.random.PRNGKey(42), N),
+        TreeBuffer(
+            lower=jnp.zeros([MAX_NODES]).at[0].set(0.0),
+            upper=jnp.zeros([MAX_NODES]).at[0].set(1.0),
+            values=jnp.zeros([MAX_NODES]),
+        ),
+    )
+
+    buffer, idx = trace.retval
 
     fig, ax = plt.subplots(1, 1)
     plt.title("Changepoint Segments")
@@ -138,36 +182,28 @@ def test_changepoint_model():
     plt.ylabel("y")
     plt.xlim(0, 1)
 
-    c = ["red", "blue"]
-    for i in range(2):
+    for i in range(N):
 
-        key = jax.random.PRNGKey(i)
-        buffer = TreeBuffer(
-            lower=jnp.zeros([MAX_NODES]).at[0].set(0.0),
-            upper=jnp.zeros([MAX_NODES]).at[0].set(1.0),
-            values=jnp.zeros([MAX_NODES]),
-        )
-        trace: Trace = binary_tree.simulate(
-            key,
-            (buffer, buffer.idx),
-        )
+        tree = tree_unflatten(buffer, i)
+        render_node(ax, tree, colors[i])
 
-        render_segments_trace(ax, trace, color=c[i])
     fig.savefig("test_changepoint_model.png")
 
 
 # cpu-side, recursive tree builder
-def tree_unflatten(tree_buffer: TreeBuffer, idx: int = 0) -> Node:
-    if tree_buffer.is_leaf[idx]:
+def tree_unflatten(tree_buffer: TreeBuffer, j: int, idx: int = 0) -> Node:
+    if tree_buffer.is_leaf[j, idx]:
         return LeafNode(
-            value=tree_buffer.values[idx],
-            interval=Interval(tree_buffer.lower[idx], tree_buffer.upper[idx]),
+            idx=idx,
+            value=tree_buffer.values[j, idx],
+            interval=Interval(tree_buffer.lower[j, idx], tree_buffer.upper[j, idx]),
         )
     else:
         return InternalNode(
-            left=tree_unflatten(tree_buffer, tree_buffer.children[idx, 0]),
-            right=tree_unflatten(tree_buffer, tree_buffer.children[idx, 1]),
-            interval=Interval(tree_buffer.lower[idx], tree_buffer.upper[idx]),
+            idx=idx,
+            left=tree_unflatten(tree_buffer, j, tree_buffer.left_idx[j, idx]),
+            right=tree_unflatten(tree_buffer, j, tree_buffer.right_idx[j, idx]),
+            interval=Interval(tree_buffer.lower[j, idx], tree_buffer.upper[j, idx]),
         )
 
 
@@ -186,10 +222,3 @@ def render_node(ax: plt.Axes, node: Node, color: str) -> None:
 
         case _:
             raise ValueError(f"Unknown node type: {type(node)}")
-
-
-def render_segments_trace(ax: plt.Axes, trace: Trace, color: str) -> None:
-    buffer, idx = trace.retval
-    tree = tree_unflatten(buffer)
-
-    render_node(ax, tree, color)
