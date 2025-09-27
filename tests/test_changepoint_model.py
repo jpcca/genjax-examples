509209269d1f293cc751464acadd6e914c6d48bc
gszep <grisha.szep@gmail.com>
2025-09-26 21:41:30 -0400

    separate data from topology in `BinaryTree`

--- tests/test_changepoint_model.py
+++ tests/test_changepoint_model.py
@@ -5,7 +5,7 @@ GenJAX changepoint model example from https://www.gen.dev/tutorials/intro-to-mod
 from abc import ABC
 from dataclasses import dataclass, field
 from functools import partial
-
+from typing import ClassVar
 
 import jax
 from jax import jit, vmap
@@ -26,13 +26,10 @@ from genjax import (
     Weight,
     GenerativeFunction,
 )
-from jaxtyping import Array, Float, Bool, Integer, PRNGKeyArray
+from jaxtyping import Array, Float, Bool, PRNGKeyArray
 
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
-
-MAX_DEPTH = 5
-MAX_NODES = 2 ** (MAX_DEPTH + 1) - 1
 
 
 @jax.tree_util.register_dataclass
@@ -65,44 +62,17 @@ class LeafNode(Node):
 
 @jax.tree_util.register_dataclass
 @dataclass
-class NodeBuffer:
-    lower: Float[Array, "MAX_NODES"]
-    upper: Float[Array, "MAX_NODES"]
-    values: Float[Array, "MAX_NODES"]
+class NodeData:
+    lower: Array
+    upper: Array
+    values: Array
 
-    idx: Integer[Array, "MAX_NODES"] = field(  # in breadth-first search order
-        default_factory=lambda: jnp.arange(MAX_NODES, dtype=jnp.int32),
-        metadata=dict(static=True),
-    )
-
-    left_idx: Integer[Array, "MAX_NODES"] = field(
-        default_factory=lambda: jnp.where(
-            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 1 < MAX_NODES,
-            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 1,
-            -1,
+    def __getitem__(self, idx: int | slice) -> "NodeData":
+        return NodeData(
+            lower=self.lower[idx],
+            upper=self.upper[idx],
+            values=self.values[idx],
         )
-    )
-
-    right_idx: Integer[Array, "MAX_NODES"] = field(
-        default_factory=lambda: jnp.where(
-            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 2 < MAX_NODES,
-            2 * jnp.arange(MAX_NODES, dtype=jnp.int32) + 2,
-            -1,
-        )
-    )
-
-    @classmethod
-    def from_array(cls, array: Float[Array, "..."]) -> "NodeBuffer":
-        init_nan = jnp.full(shape=MAX_NODES, fill_value=jnp.nan)
-        return cls(
-            lower=init_nan.at[0].set(jnp.amin(array)),
-            upper=init_nan.at[0].set(jnp.amax(array)),
-            values=init_nan,
-        )
-
-    @property
-    def is_leaf(self) -> Bool[Array, "MAX_NODES"]:
-        return (self.left_idx == -1) & (self.right_idx == -1)
 
     @property
     def shape(self) -> tuple[int, ...]:
@@ -112,47 +82,116 @@ class NodeBuffer:
     def ndim(self) -> int:
         return self.values.ndim
 
-    def __getitem__(self, idx: int | slice) -> "NodeBuffer":
-        return NodeBuffer(
-            lower=self.lower[idx],
-            upper=self.upper[idx],
-            values=self.values[idx],
-            idx=self.idx,
+
+@partial(
+    jax.tree_util.register_dataclass,
+    meta_fields=["node_idx"],
+    data_fields=["right_idx", "left_idx"],
+)
+@dataclass
+class Topology:
+    LEAF_IDX: ClassVar[int] = -1
+
+    node_idx: Array
+    left_idx: Array
+    right_idx: Array
+
+    def __getitem__(self, idx: int | slice) -> "Topology":
+        return Topology(
+            node_idx=self.node_idx,
             left_idx=self.left_idx[idx],
             right_idx=self.right_idx[idx],
         )
 
+    def to_leaf(self, idx: int | slice):
+        self.left_idx = self.left_idx.at[idx].set(self.LEAF_IDX)
+        self.right_idx = self.right_idx.at[idx].set(self.LEAF_IDX)
+
+
+@jax.tree_util.register_dataclass
+@dataclass
+class BinaryTree:
+    MAX_NODES: ClassVar[int] = 2 ** (5 + 1) - 1
+
+    topology: Topology
+    data: NodeData
+
+    @classmethod
+    def from_array(cls, array: Float[Array, "..."]) -> "BinaryTree":
+        """initialize maximum complexity tree given MAX_NODES"""
+
+        init_nan = jnp.full(shape=cls.MAX_NODES, fill_value=jnp.nan)
+        return cls(
+            topology=Topology(
+                node_idx=jnp.arange(cls.MAX_NODES, dtype=jnp.int32),
+                left_idx=jnp.where(
+                    2 * jnp.arange(cls.MAX_NODES, dtype=jnp.int32) + 1 < cls.MAX_NODES,
+                    2 * jnp.arange(cls.MAX_NODES, dtype=jnp.int32) + 1,
+                    -1,
+                ),
+                right_idx=jnp.where(
+                    2 * jnp.arange(cls.MAX_NODES, dtype=jnp.int32) + 2 < cls.MAX_NODES,
+                    2 * jnp.arange(cls.MAX_NODES, dtype=jnp.int32) + 2,
+                    -1,
+                ),
+            ),
+            data=NodeData(
+                lower=init_nan.at[0].set(jnp.amin(array)),
+                upper=init_nan.at[0].set(jnp.amax(array)),
+                values=init_nan,
+            ),
+        )
+
+    @property
+    def is_leaf(self) -> Bool[Array, "MAX_NODES"]:
+        return (self.topology.left_idx == self.topology.LEAF_IDX) & (
+            self.topology.right_idx == self.topology.LEAF_IDX
+        )
+
+    @property
+    def shape(self) -> tuple[int, ...]:
+        return self.data.shape
+
+    @property
+    def ndim(self) -> int:
+        return self.data.ndim
+
+    def __getitem__(self, idx: int | slice) -> "BinaryTree":
+        return BinaryTree(
+            topology=self.topology[idx],
+            data=self.data[idx],
+        )
+
 
 @gen
-def leaf(buffer: NodeBuffer, idx: int) -> tuple[NodeBuffer, int]:
+def leaf(buffer: BinaryTree, idx: int) -> tuple[BinaryTree, int]:
     value = normal(0.0, 1.0) @ f"value_{idx}"
-    buffer.values = buffer.values.at[idx].set(value)
+    buffer.data.values = buffer.data.values.at[idx].set(value)
 
-    buffer.left_idx = buffer.left_idx.at[idx].set(-1)
-    buffer.right_idx = buffer.right_idx.at[idx].set(-1)
+    buffer.topology.to_leaf(idx)
     return buffer, idx
 
 
 @gen
-def branch(buffer: NodeBuffer, idx: int) -> tuple[NodeBuffer, int]:
-    lower, upper = buffer.lower[idx], buffer.upper[idx]
+def branch(buffer: BinaryTree, idx: int) -> tuple[BinaryTree, int]:
+    lower, upper = buffer.data.lower[idx], buffer.data.upper[idx]
 
     frac = beta(2.0, 2.0) @ f"beta_{idx}"
     midpoint = lower + frac * (upper - lower)
-    left, right = buffer.left_idx[idx], buffer.right_idx[idx]
+    left, right = buffer.topology.left_idx[idx], buffer.topology.right_idx[idx]
 
-    buffer.lower = buffer.lower.at[left].set(lower)
-    buffer.upper = buffer.upper.at[left].set(midpoint)
+    buffer.data.lower = buffer.data.lower.at[left].set(lower)
+    buffer.data.upper = buffer.data.upper.at[left].set(midpoint)
 
-    buffer.lower = buffer.lower.at[right].set(midpoint)
-    buffer.upper = buffer.upper.at[right].set(upper)
+    buffer.data.lower = buffer.data.lower.at[right].set(midpoint)
+    buffer.data.upper = buffer.data.upper.at[right].set(upper)
 
     return buffer, idx
 
 
-@scan(n=MAX_NODES)  # in breadth-first search order
+@scan(n=BinaryTree.MAX_NODES)  # in breadth-first search order
 @gen
-def binary_tree(buffer: NodeBuffer, idx: int) -> tuple[NodeBuffer, int]:
+def binary_tree(buffer: BinaryTree, idx: int) -> tuple[BinaryTree, int]:
     args = (buffer, idx)
 
     is_leaf = flip(0.5) @ f"is_leaf_{idx}"
@@ -165,24 +204,26 @@ def binary_tree(buffer: NodeBuffer, idx: int) -> tuple[NodeBuffer, int]:
 @jit
 @partial(vmap, in_axes=(0, None))
 def binary_tree_simulate(key: PRNGKeyArray, xs: Float[Array, "..."]) -> Trace:
-    buffer = NodeBuffer.from_array(xs)
-    return binary_tree.simulate(key, args=(buffer, buffer.idx))
+    buffer = BinaryTree.from_array(xs)
+    return binary_tree.simulate(key, args=(buffer, buffer.topology.node_idx))
 
 
-def get_values_at(xs: Float[Array, "..."], buffer: NodeBuffer) -> Float[Array, "..."]:
+def get_values_at(xs: Float[Array, "..."], buffer: BinaryTree) -> Float[Array, "..."]:
     mask = (
         buffer.is_leaf
-        & (buffer.lower <= jnp.expand_dims(xs, axis=-1))
-        & (jnp.expand_dims(xs, axis=-1) <= buffer.upper)
+        & (buffer.data.lower <= jnp.expand_dims(xs, axis=-1))
+        & (jnp.expand_dims(xs, axis=-1) <= buffer.data.upper)
     )
 
-    return jnp.matmul(mask.astype(xs.dtype), jnp.nan_to_num(buffer.values, nan=0.0))
+    return jnp.matmul(
+        mask.astype(xs.dtype), jnp.nan_to_num(buffer.data.values, nan=0.0)
+    )
 
 
 @gen
-def changepoint_model(xs: Float[Array, "..."]) -> tuple[NodeBuffer, int]:
-    buffer = NodeBuffer.from_array(xs)
-    buffer, idx = binary_tree(buffer, buffer.idx) @ "binary_tree"
+def changepoint_model(xs: Float[Array, "..."]) -> tuple[BinaryTree, int]:
+    buffer = BinaryTree.from_array(xs)
+    buffer, idx = binary_tree(buffer, buffer.topology.node_idx) @ "binary_tree"
 
     noise = gamma(0.5, 0.5) @ "noise"
     normal(get_values_at(xs, buffer), noise) @ "y"
@@ -206,7 +247,7 @@ def importance_resampling(
     constraint: ChoiceMap,
     args: Arguments,
     num: int = 100_000,
-) -> tuple[NodeBuffer, Weight]:
+) -> tuple[BinaryTree, Weight]:
     key, subkey = jax.random.split(key)
     keys = jax.random.split(subkey, num)
 
@@ -219,19 +260,27 @@ def importance_resampling(
 
 
 # cpu-side, recursive tree builder
-def tree_unflatten(tree_buffer: NodeBuffer, j: int, idx: int = 0) -> Node:
+def tree_unflatten(tree_buffer: BinaryTree, j: int, idx: int = 0) -> Node:
     if tree_buffer.is_leaf[j, idx]:
         return LeafNode(
             idx=idx,
-            value=tree_buffer.values[j, idx],  # type: ignore
-            interval=Interval(tree_buffer.lower[j, idx], tree_buffer.upper[j, idx]),
+            value=tree_buffer.data.values[j, idx],  # type: ignore
+            interval=Interval(
+                tree_buffer.data.lower[j, idx], tree_buffer.data.upper[j, idx]
+            ),
         )
     else:
         return InternalNode(
             idx=idx,
-            left=tree_unflatten(tree_buffer, j, int(tree_buffer.left_idx[j, idx])),  # type: ignore
-            right=tree_unflatten(tree_buffer, j, int(tree_buffer.right_idx[j, idx])),  # type: ignore
-            interval=Interval(tree_buffer.lower[j, idx], tree_buffer.upper[j, idx]),
+            left=tree_unflatten(
+                tree_buffer, j, int(tree_buffer.topology.left_idx[j, idx])
+            ),  # type: ignore
+            right=tree_unflatten(
+                tree_buffer, j, int(tree_buffer.topology.right_idx[j, idx])
+            ),  # type: ignore
+            interval=Interval(
+                tree_buffer.data.lower[j, idx], tree_buffer.data.upper[j, idx]
+            ),
         )
 
 
@@ -255,7 +304,7 @@ def render_node(ax: Axes, node: Node, alpha: float | None = None) -> None:
 
 
 def render_segments(
-    buffer: NodeBuffer,
+    buffer: BinaryTree,
     weights: Float[Array, "..."] = None,
     return_figure: bool = False,
 ) -> None | tuple[plt.Figure, Axes]:
@@ -323,6 +372,5 @@ def test_changepoint_model_inference(seed: int = 42, noise: float = 0.1) -> None
     fig, ax = render_segments(buffer, weights, return_figure=True)
     ax.scatter(jnp.expand_dims(xs, axis=0), ys, s=3, color="k")
 
-    ax.legend()
     fig.savefig("test_changepoint_model.png")
     plt.close(fig)
